27d21521a2bf5ce1059ea77ca9f7e26b832a22cd
tee1365 <justin951124@gmail.com>
2018-08-15 20:20:42 +1200

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./js/App";
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/douban/build/">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
